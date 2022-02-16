cb447a9f8cea01a72d4f083ded204cc721cbf642
Daichi Murakami <harihitode@gmail.com>
2022-02-16 12:14:13 +0900

    update launch

--- launch.sh
+++ launch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ros2 launch tier4_autoware_launch logging_simulator.launch.xml map_path:=~/sample_map/ perception:=false planning:=false control:=false vehicle_model:=lexus sensor_model:=aip_xx1
+source install/setup.bash
+ros2 launch tier4_autoware_launch logging_simulator.launch.xml map_path:=${HOME}/sample_map/ perception:=false planning:=false control:=false vehicle_model:=lexus sensor_model:=aip_xx1
