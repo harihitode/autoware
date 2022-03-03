3bb5783c97a7772c41e017a76d10fa9ab45f9be5
Daichi Murakami <harihitode@gmail.com>
2022-03-03 13:12:36 +0900

    update launcher file location

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source install/setup.bash
-ros2 launch tier4_autoware_launch logging_simulator.launch.xml map_path:=${HOME}/sample_map/ perception:=false planning:=false control:=false vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
+ros2 launch autoware_launch logging_simulator.launch.xml map_path:=${HOME}/sample_map/ perception:=false planning:=false control:=false vehicle_simulation:=true vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
